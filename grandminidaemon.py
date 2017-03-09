65dce37dcac81ffcf07135ec3e6cdf104db3697a
Dan Cassidy <dan.cassidy.1983@gmail.com>
2017-03-09 16:05:32 -0500

    Fixed sleep call.

--- grandminidaemon.py
+++ grandminidaemon.py
@@ -1,8 +1,12 @@
 import time
 import pigpio
+import sys
 
 def mycb(x, y, z):
     print(time.time())
 
-pin = pigpio.pi()
-cb = pin.callback(23, pigpio.FALLING_EDGE, mycb)
+def main():
+    pin = pigpio.pi()
+    cb = pin.callback(23, pigpio.FALLING_EDGE, mycb)
+    while(True):
+        time.sleep(10)
