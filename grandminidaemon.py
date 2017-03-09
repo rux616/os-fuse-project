b092a33d1d9ffdf40fff94dfeb45de96736c0bfd
Dan Cassidy <dan.cassidy.1983@gmail.com>
2017-03-09 17:12:57 -0500

    Adding file handling.

--- grandminidaemon.py
+++ grandminidaemon.py
@@ -1,12 +1,29 @@
+import sys
 import time
 import pigpio
-import sys
 
-def mycb(x, y, z):
+def pigpio_callback(x, y, z):
     print(time.time())
 
-def main():
-    pin = pigpio.pi()
-    cb = pin.callback(23, pigpio.FALLING_EDGE, mycb)
+def signal_handler(signal, frame):
+
+
+def main(output_file):
+    # open file
+    try:
+        file_handle = open(output_file, "a")
+    except expression as identifier:
+        sys.exit()
+
+    # set up signal handler
+
+    # set up pigpio
+    pigpio_connection = pigpio.pi()
+    cb = pigpio_connection.callback(23, pigpio.FALLING_EDGE, pigpio_callback)
+
+    # loop forever
     while(True):
-        time.sleep(10)
+        time.sleep(30)
+
+if __name__ == '__main__':
+    main(sys.argv[1])
