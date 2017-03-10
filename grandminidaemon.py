50a303054c90c638d191ba1d3c7880372bd0d536
Dan Cassidy <dan.cassidy.1983@gmail.com>
2017-03-10 10:24:28 -0500

    got rid of main function
    put everything in the default indent level of the file

--- grandminidaemon.py
+++ grandminidaemon.py
@@ -3,23 +3,23 @@ import time
 import pigpio
 
 def pigpio_callback(x, y, z):
-    print(time.time())
+    time_triggered = time.time()
+    print(time_triggered)
+    file_handle.write(time_triggered)
+    file_handle.flush()
 
-def main(output_file):
-    # open file
-    try:
-        file_handle = open(output_file, "a")
-    except IOError:
-        print("Error opening file ", output_file)
-        sys.exit()
+# open file
+output_file = sys.argv[1]
+try:
+    file_handle = open(output_file, "a")
+except IOError:
+    print("Error opening file ", output_file)
+    sys.exit()
 
-    # set up pigpio
-    pigpio_connection = pigpio.pi()
-    cb = pigpio_connection.callback(23, pigpio.FALLING_EDGE, pigpio_callback)
+# set up pigpio
+pigpio_connection = pigpio.pi()
+cb = pigpio_connection.callback(23, pigpio.FALLING_EDGE, pigpio_callback)
 
-    # loop forever
-    while(True):
-        time.sleep(30)
-
-if __name__ == '__main__':
-    main(sys.argv[1])
+# loop forever
+while(True):
+    time.sleep(30)
