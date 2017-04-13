2915c4f55a8d016795f8731e92faac91e5e29646
LINQtothepast <LINQtothepast@gmail.com>
2017-04-13 19:14:01 -0400

    rand calcs upload 2

--- Rand Calcs.py
+++ Rand Calcs.py
@@ -10,16 +10,28 @@ def importStamps():
 def calcNumbers(stamps):
     print(stamps[0])
 
+    randomNums = []
+    subtractionList = []
+
     i = 0
-    for x in range(len(stamps) - 1):
+    for x in range(len(stamps) - 2):
         stamps[i] = float(stamps[i + 1]) - float(stamps[i])
-        stamps[i] = round(float(stamps[i]), 2)
+        subtractionList.append(round(float(stamps[i]), 2))
         i += 1
-    stamps.pop()
 
-    print(stamps[0])
-    return stamps
 
+    for x in subtractionList:
+        print(x)
+
+    i = 0
+    for x in range(len(subtractionList) - 1):
+        if float(subtractionList[i]) > float(subtractionList[i+1]):
+            randomNums.append(1)
+        else:
+            randomNums.append(0)
+        i += 1
+
+    return randomNums
 
 list = importStamps()
 numbers = calcNumbers(list)
