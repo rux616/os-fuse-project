460fc012833a97177e3c0036d43483c6763c82c0
Dan Cassidy <dan.cassidy.1983@gmail.com>
2017-04-18 02:41:31 -0400

    Got bored and did a lot of prettifying of the file, as well as changed some variable names to align with the python naming conventions.

--- MyFuse.py
+++ MyFuse.py
@@ -2,11 +2,11 @@
 #-*- coding: utf-8 -*-
                # encode/decode w/ utf-8: full-range of unicode chars, w/o addressing endian issues
 import errno   # makes available errno system calls
-import fuse    # include fuse calls from python-fuse
 import stat    # constants/functions for interpreting results of os.stat()
 import os      # needed for os interfaces
 import sys     # provides access to variables used/maintained by interpreter (inc. argv)
 import time    # gives time functions
+import fuse    # include fuse calls from python-fuse
 
 fuse.fuse_python_api = (0, 2)   # application programming interface (0, 2)
 
@@ -16,15 +16,15 @@ class MyFuse(fuse.Fuse):
 
     # Initializes the filesystem
     def __init__(self, *args, **kw):    # OOP constructor
-        for x in sys.argv:              # print for debugging
-            print " *** " + x           # print all arguments in sequence
+        for args in sys.argv:              # print for debugging
+            print " *** " + args           # print all arguments in sequence
 
-        # require standardized call with root directory and mountpoint
+        # require standardized call with root directory and mount_point
         if (len(sys.argv) < 3) or (sys.argv[-2][0] == '-') or (sys.argv[-1][0] == '-'):
             print "Error, proper input:"
-            print "      MyFuse [-options] rootDir mountPoint"
+            print "      MyFuse [-options] root_dir mount_point"
             print "or interpreting script:"
-            print "      python MyFuse.py [-options] rootDir mountPoint"
+            print "      python MyFuse.py [-options] root_dir mount_point"
             sys.exit(0)
 
         # check to see if the root dir and mount point are valid
@@ -36,13 +36,13 @@ class MyFuse(fuse.Fuse):
             print "Error: '" + sys.argv[-1] + "' is not a directory."
             must_exit = True
         if sys.argv[-2] == sys.argv[-1]:
-            print "Error: rootDir and mountPoint cannot be the same."
+            print "Error: root_dir and mount_point cannot be the same."
             must_exit = True
         if must_exit is True:
             sys.exit(0)
 
-        self.rootDir = sys.argv[-2]
-        print ":--- " + self.rootDir + " ---:"  # prints second to last argument/rootDir"
+        self.root_dir = sys.argv[-2]
+        print ":--- " + self.root_dir + " ---:" # prints second to last argument/root_dir"
         self.open_files = {}                    # initialize open_files to empty
         fuse.Fuse.__init__(self, *args, **kw)   # creates fuse Object
 
@@ -52,7 +52,7 @@ class MyFuse(fuse.Fuse):
             current_mode = stat.S_IMODE(os.lstat(sys.argv[-1] + dummy_touch).st_mode)
             os.chmod(sys.argv[-1] + dummy_touch, current_mode & \
               ~(stat.S_IWUSR | stat.S_IWGRP | stat.S_IWOTH))
-            self.fileStat = os.lstat(sys.argv[-1] + dummy_touch)
+            self.file_stat = os.lstat(sys.argv[-1] + dummy_touch)
         os.remove(sys.argv[-1] + dummy_touch)
 
   # ==================== Filesystem Methods ====================
@@ -61,31 +61,31 @@ class MyFuse(fuse.Fuse):
     def chmod(self, path, mode):             # defines chmod to alter permissions
         print "*** CHMOD: ", path            # print information used for debugging
         if path == "/" + self.randomFilename or path == "/" + self.cpmFilename:
-            toReturn = -errno.EPERM
+            to_return = -errno.EPERM
         else:
-            os.chmod(self.rootDir + path, mode)  # syscall updating permissions mode
-            toReturn = 0
-        return toReturn
+            os.chmod(self.root_dir + path, mode)  # syscall updating permissions mode
+            to_return = 0
+        return to_return
 
     # Obtains file attributes - method fills in the elements of the "stat" structure.
-    def getattr(self, path):                  # defines getattr to fetch attribute from file
-        print "GETATTR-path: ", path          # print information used for debugging
+    def getattr(self, path):            # defines getattr to fetch attribute from file
+        print "GETATTR-path: ", path    # print information used for debugging
         if path == "/" + self.randomFilename or path == "/" + self.cpmFilename:
-            toReturn = self.fileStat
+            to_return = self.file_stat
         else:
-            toReturn = os.lstat(self.rootDir + path)
-        return toReturn                       # return object with syscall updating status attributes
+            to_return = os.lstat(self.root_dir + path)
+        return to_return                # return object with syscall updating status attributes
 
     # Lists directory entries (dirent) back to a caller
-    def readdir(self, path, offset):   # defines readdir which lists files/folders
-        print "\n\n*** READDIR: ", path    # print information used for debugging
-        print "=====  " + self.rootDir + "  ====="  # print information used for debugging
-        for e in '.', '..':
-            yield fuse.Direntry(e)
-        for x in os.listdir(self.rootDir + path):     # list all file basenames in path
-            yield fuse.Direntry(os.path.basename(x))  # yield fuse.Direntry(os.path.basename(x))
-        for e in self.randomFilename, self.cpmFilename:
-            yield fuse.Direntry(e)
+    def readdir(self, path, offset):                # defines readdir which lists files/folders
+        print "\n\n*** READDIR: ", path             # print information used for debugging
+        print "=====  " + self.root_dir + "  =====" # print information used for debugging
+        for folders in '.', '..':
+            yield fuse.Direntry(folders)
+        for files in os.listdir(self.root_dir + path):      # list all file basenames in path
+            yield fuse.Direntry(os.path.basename(files))    # yield fuse.Direntry(os.path.basename(x))
+        for files in self.randomFilename, self.cpmFilename:
+            yield fuse.Direntry(files)
         return
 
     # Removes empty directory of specified name
@@ -101,31 +101,31 @@ class MyFuse(fuse.Fuse):
         return 0
 
     # Remove file/link/node. Unlinking of hard links removes data from last hardlink removed
-    def unlink(self, path):          # defines unlink to rmdir to remove directories
-        print "*** UNLINK: ", path   # print information used for debugging
+    def unlink(self, path):                 # defines unlink to rmdir to remove directories
+        print "*** UNLINK: ", path          # print information used for debugging
         if path == "/" + self.randomFilename or path == "/" + self.cpmFilename:
-            toReturn = -errno.EPERM     # return an "operation not permitted" error
-        elif path in self.open_files:   # error if path is open
+            to_return = -errno.EPERM        # return an "operation not permitted" error
+        elif path in self.open_files:       # error if path is open
             print "File must be closed to unlink, release open files: "
-            for x in self.open_files:   # list open files
-                print x
-            toReturn -errno.EACCES      # return an "access denied"" error
+            for files in self.open_files:   # list open files
+                print files
+            to_return = -errno.EACCES       # return an "access denied"" error
         else:
-            os.unlink(self.rootDir + path)  # syscall to unlink
-            toReturn = 0
-        return toReturn
+            os.unlink(self.root_dir + path) # syscall to unlink
+            to_return = 0
+        return to_return
 
-    # Rename a file/directory from a location to a new name or location (file can be given a new directory)
-    def rename(self, oldpath, newpath):   # defines renaming/moving method for files
-        print "*** RENAME: ", oldpath     # print oldpath information used for debugging
-        print "***     TO: ", newpath     # print newpath information used for debugging
+    # Rename a file/directory from a location to a new name or location
+    def rename(self, oldpath, newpath):     # defines renaming/moving method for files
+        print "*** RENAME: ", oldpath       # print oldpath information used for debugging
+        print "***     TO: ", newpath       # print newpath information used for debugging
         if oldpath == "/" + self.randomFilename or oldpath == "/" + self.cpmFilename or \
           newpath == "/" + self.randomFilename or newpath == "/" + self.cpmFilename:
-            toReturn = -errno.EPERM       # return an "operation not permitted" error
+            to_return = -errno.EPERM        # return an "operation not permitted" error
         else:
-            os.rename(self.rootDir + oldpath, self.rootDir + newpath)   # syscall to rename file
-            toReturn = 0
-        return toReturn
+            os.rename(self.root_dir + oldpath, self.root_dir + newpath)   # syscall to rename file
+            to_return = 0
+        return to_return
 
     # ==================== File Methods ====================
 
@@ -133,69 +133,69 @@ class MyFuse(fuse.Fuse):
     def create(self, path, flags, mode):                # defines method to create file
         print "*** CREATE: ", path                      # print for debugging
         if path == "/" + self.randomFilename or path == "/" + self.cpmFilename:
-            toReturn = -errno.EPERM                     # return an "operation not permitted" error
+            to_return = -errno.EPERM                    # return an "operation not permitted" error
         else:
-            fhandle = open(self.rootDir + path, "w")    # opens new file to write to
+            fhandle = open(self.root_dir + path, "w")   # opens new file to write to
             self.open_files[path] = fhandle             # open new file
-            toReturn = 0
-        return toReturn
+            to_return = 0
+        return to_return
 
     # Opens a file to read or write
-    def open(self, path, flags):   # defines method to open a file
-        print "*** OPEN: ", path  # print information used for debugging
+    def open(self, path, flags):    # defines method to open a file
+        print "*** OPEN: ", path    # print information used for debugging
         print "---               ---"
 
         access_flags = os.O_RDONLY | os.O_WRONLY | os.O_RDWR    # set access flags
         access_flags = flags & access_flags
 
         if access_flags == os.O_RDONLY:           # if file is "read only"
-            fi = open(self.rootDir + path, "rb")  # file opened in "read only/binary" mode
-            self.open_files[path] = fi            # open file moved to open_files list
+            file_handle = open(self.root_dir + path, "rb")  # file opened in "read only/binary" mode
+            self.open_files[path] = file_handle            # open file moved to open_files list
             return 0
         else:                                     # if file is "write only" or "read & write"
-            fi = open(self.rootDir + path, "wb")  # file opened in "write only/binary" mode
-            self.open_files[path] = fi            # open file moved to open_files list
+            file_handle = open(self.root_dir + path, "wb")  # file opened in "write only/binary" mode
+            self.open_files[path] = file_handle            # open file moved to open_files list
             return 0
-        return -errno.EACCESS  # if file doesn't fit prior criteria, error: permission denied
+        return -errno.EACCES  # if file doesn't fit prior criteria, error: permission denied
 
     # Reads file data to buffer, beginning at offset in a file.
-    def read(self, path, size, offset):  # defines method to read file
+    def read(self, path, size, offset): # defines method to read file
         print "*** READ: ", path
         print "**  size: ", size
         print "* offset: ", offset      # print information used for debugging
 
-        fhread = self.open_files[path]  # initialize variable with opened file
-        fhread.seek(offset)             # seek/find location in file to read data from
-        return fhread.read(size)      # returns reading from a given length
+        file_handle = self.open_files[path]  # initialize variable with opened file
+        file_handle.seek(offset)             # seek/find location in file to read data from
+        return file_handle.read(size)        # returns reading from a given length
 
     # Writes file data from buffer beginning at file start offset
     def write(self, path, buf, offset, fh = None):  # defines method to write to a file
         print "*** WRITE: ", path
-        print "** offset: ", offset       # print information used for debugging
+        print "** offset: ", offset     # print information used for debugging
 
-        fhwrite = self.open_files[path]   # set output file to value
-        fhwrite.seek(offset)              # find/seek location (aka offset) from file start
-        fhwrite.write(buf)                # write to file from buffer
-        return len(buf)                   # return buffer length
+        file_handle = self.open_files[path] # set output file to value
+        file_handle.seek(offset)            # find/seek location (aka offset) from file start
+        file_handle.write(buf)              # write to file from buffer
+        return len(buf)                 # return buffer length
 
     # Called on each close so that the filesystem has a chance to report delayed errors.
-    def flush(self, path, fh = None):         # defines flush method for Fuse
-        print "*** FLUSH: ", path      # print information used for debugging
-        if path in self.open_files:    # if path is open, flush the buffer
+    def flush(self, path, fh = None):   # defines flush method for Fuse
+        print "*** FLUSH: ", path       # print information used for debugging
+        if path in self.open_files:     # if path is open, flush the buffer
             fh = self.open_files[path]
             fh.flush()
         return 0
 
     # Closes files and frees-up allocated space
-    def release(self, path, fh = None):  # releases files and re-allocates used space
-        print "*** RELEASE: ", path      # print information used for debugging
-        if path in self.open_files:      # if there are open files listed
-            fh = self.open_files[path]   # open file assigned to filehandler and closed
+    def release(self, path, fh = None): # releases files and re-allocates used space
+        print "*** RELEASE: ", path     # print information used for debugging
+        if path in self.open_files:     # if there are open files listed
+            fh = self.open_files[path]  # open file assigned to filehandler and closed
             fh.close()
-            del self.open_files[path]    # delete/reallocate space from open file
+            del self.open_files[path]   # delete/reallocate space from open file
         return 0
 
 if __name__ == '__main__':
-    fs = MyFuse()
-    fs.parse(errex=1)
-    fs.main()
+    FS = MyFuse()
+    FS.parse(errex=1)
+    FS.main()
