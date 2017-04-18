9244cbb243733ac3dd4da239ef323c381dab84b8
Dan Cassidy <dan.cassidy.1983@gmail.com>
2017-04-17 23:02:01 -0400

    Removed various trailing whitespace to make my linter happier.

--- MyFuse.py
+++ MyFuse.py
@@ -2,7 +2,7 @@
 #-*- coding: utf-8 -*-
                # encode/decode w/ utf-8: full-range of unicode chars, w/o addressing endian issues
 import errno   # makes available errno system calls
-import fuse    # include fuse calls from python-fuse        
+import fuse    # include fuse calls from python-fuse
 import stat    # constants/functions for interpreting results of os.stat()
 import os      # needed for os interfaces
 import sys     # provides access to variables used/maintained by interpreter (inc. argv)
@@ -12,7 +12,7 @@ fuse.fuse_python_api = (0, 2)   # application programming interface (0, 2)
 
 class MyFuse(fuse.Fuse):
     randomFilename = "grandom"      # variable to hold the filename for random number access
-    filenameCPM = "gcpm"            # variable to hold the filename for CPM access
+    cpmFilename = "gcpm"            # variable to hold the filename for CPM access
 
     # Initializes the filesystem
     def __init__(self, *args, **kw):    # OOP constructor
@@ -21,7 +21,7 @@ class MyFuse(fuse.Fuse):
 
         # require standardized call with root directory and mountpoint
         if (len(sys.argv) < 3) or (sys.argv[-2][0] == '-') or (sys.argv[-1][0] == '-'):
-            print "Error, proper input:" 
+            print "Error, proper input:"
             print "      MyFuse [-options] rootDir mountPoint"
             print "or interpreting script:"
             print "      python MyFuse.py [-options] rootDir mountPoint"
@@ -60,31 +60,31 @@ class MyFuse(fuse.Fuse):
         if path == "/" + self.randomFilename:      # handle the random file
             to_return = os.lstat("/dev/random")
             # to_return
-        elif path == "/" + self.filenameCPM:       # 
+        elif path == "/" + self.cpmFilename:       # 
             to_return = os.lstat("/dev/random")
             # to_return
         else:
             to_return = os.lstat(self.rootDir + path)
         return to_return                      # return object with syscall updating status attributes
-    
+
     # Lists directory entries (dirent) back to a caller
     def readdir(self, path, offset):   # defines readdir which lists files/folders
         print "\n\n*** READDIR: ", path    # print information used for debugging
-        print "=====  " + self.rootDir + "  ====="  # print information used for debugging   
+        print "=====  " + self.rootDir + "  ====="  # print information used for debugging
         for e in '.', '..':
             yield fuse.Direntry(e)
         for x in os.listdir(self.rootDir + path):     # list all file basenames in path
             yield fuse.Direntry(os.path.basename(x))  # yield fuse.Direntry(os.path.basename(x))
-        for e in self.randomFilename, self.filenameCPM:
+        for e in self.randomFilename, self.cpmFilename:
             yield fuse.Direntry(e)
         return
-    
+
     # Removes empty directory of specified name
     def rmdir(self, path):         # defines rmdir to remove directories
         print "*** RMDIR: ", path  # print information used for debugging
         os.rmdir(self.path)        # syscall to remove directory path
         return 0
-    
+
     # Creates a new directory of specified name
     def mkdir(self, path, mode):   # defines mkdir to create new directories
         print "*** MKDIR: ", path  # print information used for debugging
@@ -101,14 +101,14 @@ class MyFuse(fuse.Fuse):
             return -errno.ENOSYS        # error function not implemented
         os.unlink(self.rootDir + path)  # syscall to unlink
         return 0
-    
+
     # Rename a file/directory from a location to a new name or location (file can be given a new directory)
     def rename(self, oldpath, newpath):   # defines renaming/moving method for files
-        print "*** RENAME: ", oldpath     # print oldpath information used for debugging  
+        print "*** RENAME: ", oldpath     # print oldpath information used for debugging
         print "***     TO: ", newpath     # print newpath information used for debugging
         os.rename(self.rootDir + oldpath, self.rootDir + newpath)   # syscall to rename file
         return 0
-                 
+
     # ==================== File Methods ====================
 
     # creates path not previously in existence
@@ -117,15 +117,15 @@ class MyFuse(fuse.Fuse):
         fhandle = open(self.rootDir + path, "w")    # opens new file to write to
         self.open_files[path] = fhandle             # open new file
         return 0
-    
+
     # Opens a file to read or write
     def open(self, path, flags):   # defines method to open a file
         print "*** OPEN: ", path  # print information used for debugging
         print "---               ---"
-        
+
         access_flags = os.O_RDONLY | os.O_WRONLY | os.O_RDWR    # set access flags
         access_flags = flags & access_flags
-        
+
         if access_flags == os.O_RDONLY:           # if file is "read only"
             fi = open(self.rootDir + path, "rb")  # file opened in "read only/binary" mode
             self.open_files[path] = fi            # open file moved to open_files list
@@ -141,39 +141,39 @@ class MyFuse(fuse.Fuse):
         print "*** READ: ", path
         print "**  size: ", size
         print "* offset: ", offset      # print information used for debugging
-        
+
         fhread = self.open_files[path]  # initialize variable with opened file
         fhread.seek(offset)             # seek/find location in file to read data from
         return fhread.read(size)      # returns reading from a given length
 
-    # Writes file data from buffer beginning at file start offset           
+    # Writes file data from buffer beginning at file start offset
     def write(self, path, buf, offset, fh = None):  # defines method to write to a file
         print "*** WRITE: ", path
         print "** offset: ", offset       # print information used for debugging
-        
+
         fhwrite = self.open_files[path]   # set output file to value
         fhwrite.seek(offset)              # find/seek location (aka offset) from file start
         fhwrite.write(buf)                # write to file from buffer
         return len(buf)                   # return buffer length
 
-    # Called on each close so that the filesystem has a chance to report delayed errors.             
+    # Called on each close so that the filesystem has a chance to report delayed errors.
     def flush(self, path, fh = None):         # defines flush method for Fuse
         print "*** FLUSH: ", path      # print information used for debugging
         if path in self.open_files:    # if path is open, flush the buffer
             fh = self.open_files[path]
-            fh.flush() 
+            fh.flush()
         return 0
 
-    # Closes files and frees-up allocated space            
+    # Closes files and frees-up allocated space
     def release(self, path, fh = None):  # releases files and re-allocates used space
         print "*** RELEASE: ", path      # print information used for debugging
         if path in self.open_files:      # if there are open files listed
             fh = self.open_files[path]   # open file assigned to filehandler and closed
-            fh.close()                               
+            fh.close()
             del self.open_files[path]    # delete/reallocate space from open file
         return 0
 
 if __name__ == '__main__':
     fs = MyFuse()
-    fs.parse(errex = 1)
+    fs.parse(errex=1)
     fs.main()
