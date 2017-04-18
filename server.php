2f750f68c3b05e220f205b53182d38bc997c6a27
rhindaniloyalar <rhindani@live.com>
2017-04-18 16:38:18 -0400

    finished php script

--- server.php
+++ server.php
@@ -1,28 +1,44 @@
 <?php
+//saves the fpointer to a file, need a seperate file to timestamps for this.
 
 $buffer = [];
-$fpointer = 0;
-$numStamps = $_GET['numStamps'];
+$numStamps = 5; //$_GET['numStamps'];
+$j = 0;
 
-$handle = popen("", "r");
-	if ($handle)
+$handle = fopen("timestamps.txt", "r") 
+	or die ("Error: could not open timestamps!\n");
+$write = fopen("fpointer.txt", "r+") 
+	or die ("could not open fpointer");
+
+$fpointer = fgets($write);
+
+	
+	if ($handle and $write)
 	{
+		fseek($handle, $fpointer, SEEK_SET);  //set file pointer
+		
 		for ($i = $numStamps; $i > 0; --$i)
 		{
-			$buffer = fgets($handle, 32);
-			echo "moving ftell " . ftell($handle);
+			$buffer[$j] = fgets($handle, 32);
+			echo "moving ftell: " . ftell($handle);
+			echo "<br> <br>";
+			$j = $j + 1;
 		}
 		
-		$fpointer  = ftell($handle);
+		$write = fopen("fpointer.txt", "w") or die ("failed to open fpointer");	
+		
+		$fpointer = ftell($handle);
+		fwrite($write, $fpointer);
 		
 		echo ftell($handle);
-		echo "Final destination " . $fpointer;
-		echo $buffer;
+		echo " <br> Final destination <br> " . $fpointer;
+		echo "<br>";
 		
-		pclose($handle);
-	}
-	else
-	{
-		echo "Error: could not open file!\n";
-	}			
+		print_r(array_values($buffer));
+
+		fclose($handle);
+		fclose($write);
+		
+		return $buffer;
+	}	
 ?>
