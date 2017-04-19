37600af0da4de1d5f12f092d95502ab1146be0bd
LINQtothepast <LINQtothepast@gmail.com>
2017-04-18 23:53:00 -0400

    Rand Calcs 5.2

--- Rand Calcs.py
+++ Rand Calcs.py
@@ -3,30 +3,30 @@ import sys
 import random
 import urllib2
 
-x =10
-result = urllib2.urlopen("http://cs.iusb.edu/~djcassid/server.php?numStamps=" + x) .read()
-print(result)
 
-def importStamps():
-    return [line.rstrip('\n') for line in open('TimeStamps.txt')]
+#does a webcall to a php file asking for x number of stamps
+#then stores the time stamps into a list to be used by later functions
+def importStamps(x):
+    words = urllib2.urlopen("http://cs.iusb.edu/~djcassid/server.php?numStamps=" + str(x)) .read()
+    words = words.strip().split()
+    #print(words[0])
+    return words
 
 
+#takes the list of stamps and current spot in the list
+#gets subtracted from the next stamp in the list
+#this represents the time elapsed which is stored in a new list subtractionList
+#this new list then does a comparison of elapsed times where if the
+#current is greater we get a 1 else we get a 0
 def calcNumbers(stamps):
-    #print(stamps[0])
-
     randomNums = []
     subtractionList = []
-    #print(len(stamps))
     i = 0
     for x in range(len(stamps) - 1):
         stamps[i] = float(stamps[i + 1]) - float(stamps[i])
         subtractionList.append(round(float(stamps[i]), 2))
         i += 1
-    stamps.clear()
 
-    #for x in subtractionList:
-    #    print(x)
-    #print(len(subtractionList))
     i = 0
     for x in range(len(subtractionList) -1):
         if float(subtractionList[i]) > float(subtractionList[i+1]):
@@ -34,13 +34,12 @@ def calcNumbers(stamps):
         else:
             randomNums.append(0)
         i += 1
-    subtractionList.clear
     return randomNums
 
-
+#here we simply create a unpacked byte number but its called
+#packed because logically we packed 8 bits together
 def packBits(BitList):
     packedList = []
-    #print(len(BitList))
     for x in range(0,len(BitList)-7,8):
         #iterates 0 to length of BitList on steps of 8
         packedList.append(str(BitList[x])+str(BitList[x+1]) +
@@ -48,12 +47,9 @@ def packBits(BitList):
             str(BitList[x + 4]) + str(BitList[x + 5]) +
             str(BitList[x + 6]) + str(BitList[x + 7]))
 
-    #for x in packedbits:
-    #   print(x)
-    BitList.clear()
     return packedList
 
-
+#here we simply turn a byte into a normal integer
 def unpackBits (packedList):
     numList = []
     tempList = []
@@ -68,10 +64,6 @@ def unpackBits (packedList):
         tempList.append(tempStr[8])
         tempList.append(tempStr[9])
 
-    #for x in tempList2:
-    #   print(x)
-    #print(len(tempList2))
-
     for x in range(0, len(tempList) - 7, 8):
         sum = 0
         sum = sum + (int(int(tempList[x + 7])))
@@ -84,22 +76,23 @@ def unpackBits (packedList):
         sum = sum + (int(int(tempList[x])) * 128)
         numList.append(sum)
 
-    #for x in numList:
-    #    print(x)
-
-    tempList.clear()
     return numList
 
+#here we make a call to cpm.php to get 60 stamps that we then calculate the
+#total time elapsed and throw out any timestamp that is more than 30 seconds
+#after the last one. Then we take that time and convert from seconds to minutes
+#and divide the count of our stamps by the total minutes to find the average
+#counts per minute
 def calcTime():
-    stamps = importStamps()
+    stamps = urllib2.urlopen("http://cs.iusb.edu/~djcassid/cpm.php").read()
+    stamps = stamps.strip().split()
     subtractionList = []
-    #print(len(stamps))
+
     i = 0
     for x in range(len(stamps) - 1):
         stamps[i] = float(stamps[i + 1]) - float(stamps[i])
         subtractionList.append(round(float(stamps[i]), 2))
         i += 1
-    stamps.clear()
 
     count = 1
     timeSum = 0
@@ -110,33 +103,41 @@ def calcTime():
             timeSum = timeSum + subtractionList[i]
         i += 1
 
-    print(count)
-    print(timeSum)
-
-    minutesCount = int(timeSum / 60)
-    print(minutesCount)
-    cpm = count / minutesCount
-    print(cpm)
+    minutesCount = float(timeSum / 60)
+    cpm = round(float(count / minutesCount), 2)
+    print("total time in seconds = " + str(timeSum))
+    print("total number of timestamps counted = " + str(count))
+    print("timestamp count per minute = " + str(cpm))
 
     return cpm
 
-def packedListCall():
-    unpackedList = importStamps()
-    unpackedList = calcNumbers(unpackedList)
-    unpackedList = packBits(unpackedList)
-    unpackedList = unpackBits(unpackedList)
-    return unpackedList
+#calls functions to return an integer list
+def packedListCall(x):
+    unpackedList = []
+    try:
+        unpackedList = importStamps((x*8)+2)
+        unpackedList = calcNumbers(unpackedList)
+        unpackedList = packBits(unpackedList)
+        unpackedList = unpackBits(unpackedList)
+        return unpackedList
+    except:
+        return unpackedList
 
-def unpackedListCall():
-    packedList = importStamps()
-    packedList = calcNumbers(packedList)
-    packedList = packBits(packedList)
-    return packedList
+
+#calls functions to return a list of bytes
+def unpackedListCall(x):
+    packedList = []
+    try:
+        packedList = importStamps((x*8)+2)
+        packedList = calcNumbers(packedList)
+        packedList = packBits(packedList)
+        return packedList
+    except:
+        return packedList
 
 #example main calls
-list = unpackedListCall()
-list2 = packedListCall()
+list = unpackedListCall(1)
+list2 = packedListCall(1)
 print(list[0])
 print(list2[0])
-
-var1 = calcTime()
+calcTime()
