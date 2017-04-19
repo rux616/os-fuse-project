7af8447bcd80cab957a6f91419485bd15d5da15a
rhindaniloyalar <rhindani@live.com>
2017-04-18 20:50:12 -0400

    Toss out first timestamp

--- cpm.php
+++ cpm.php
@@ -1,12 +1,14 @@
 <?php
 $handle = fopen("timestamps.txt", "r") or die ("Could not open timestamps file");
 $buffer = "";
-const NUM_STAMPS = 60;
+const NUM_STAMPS = 61;
 $fpointer;
 
 if ($handle)
 {
     $fpointer = fseek($handle, -900, $whence = SEEK_END);
+    fgets($handle);
+    #$buffer = "";               #toss out first timestamp
 
     for($i = 0; $i < NUM_STAMPS; ++$i)
     {
