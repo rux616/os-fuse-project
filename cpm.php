cd172eea199dee10d1ead7f5ac88bcd60170916d
rhindaniloyalar <rhindani@live.com>
2017-04-18 20:17:05 -0400

    cpm fix

--- cpm.php
+++ cpm.php
@@ -1,18 +1,21 @@
 <?php
 $handle = fopen("timestamps.txt", "r") or die ("Could not open timestamps file");
-$buffer = [];
+$buffer = "";
+const NUM_STAMPS = 60;
 $fpointer;
 
 if ($handle)
 {
     $fpointer = fseek($handle, -900, $whence = SEEK_END);
 
-    for($i = 0; i < 60; ++i)
+    for($i = 0; $i < NUM_STAMPS; ++$i)
     {
-        $buffer[$i] = fgets($handle, 32);
+        $buffer .= fgets($handle, 32);
     }
 
     fclose($handle);
+
+    print_r($buffer);
     
     return $buffer;
 }
